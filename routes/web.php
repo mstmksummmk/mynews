a8e6733a8c0b4cbef42cc520de5c258e860c575a
masato_yamada <posse_ymd@yahoo.co.jp>
2021-09-19 11:39:34 +0000

    Blade File HTML

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,9 @@ Route::get('/', function () {
 
 Route::group(["prefix"=>"admin"], function(){
   Route::get("news/create", "Admin\NewsController@add");
+  Route::get('profile/create', 'Admin\ProfileController@add');
+  Route::get('profile/edit', 'Admin\ProfileController@edit');
+
 });
+
+Route::get('XXX', 'AAAController@bbb');
